30d167c0968f919e9bf2640e653637186a5eae67
ansmeer008 <ansmeer008@gmail.com>
2023-04-15 12:49:48 +0900

    feat: edit ItemComment Component

--- src/components/ItemComment.vue
+++ src/components/ItemComment.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Comment space</div>
+  <div class="commentContainer">
+    <p>댓글 기능은 준비 중입니다...</p>
+  </div>
 </template>
 
 <script>
@@ -8,4 +10,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.commentContainer {
+  background-color: rgb(244, 244, 244);
+  margin-top: 40px;
+  padding: 8px;
+  border-radius: 10px;
+}
+</style>
