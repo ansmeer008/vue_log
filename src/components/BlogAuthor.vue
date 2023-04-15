9b5feb906eddb132961fa46192ed895053a7f5a0
ansmeer008 <ansmeer008@gmail.com>
2023-04-15 12:49:35 +0900

    feat: edit BlogAuthor Component

--- src/components/BlogAuthor.vue
+++ src/components/BlogAuthor.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>작가 소개</div>
+  <div class="authorContainer">
+    <div>블로그 주인 소개</div>
+    <p>매일 매일 한 걸음씩 더 나아가고자 하는 프론트엔드 개발자 Jade 입니다!</p>
+  </div>
 </template>
 
 <script>
@@ -8,4 +11,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.authorContainer {
+  background-color: rgb(244, 244, 244);
+  margin-top: 40px;
+  padding: 8px;
+  border-radius: 10px;
+}
+p {
+  margin: 0;
+}
+</style>
