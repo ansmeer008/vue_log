27fc64e4fa1d10b4307131c435d4503dc28c08d8
ansmeer008 <ansmeer008@gmail.com>
2023-04-15 11:36:39 +0900

    feat: BlogIntro Component

---
+++ src/components/BlogIntro.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mt-5">
+    <h5>Frontend 개발자 Jade의 블로그입니다!</h5>
+    <p>~Vue 공부하는 중!~</p>
+    <img
+      class="blogImg"
+      src="../assets/ham.jpeg"
+      alt="img for introduce this blog"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogIntro",
+};
+</script>
+
+<style></style>
