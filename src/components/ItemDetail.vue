94ce7403f27b64a96a01ee741e047506ea8ca52e
ansmeer008 <ansmeer008@gmail.com>
2023-04-15 11:37:05 +0900

    feat: ItemDetail Component

---
+++ src/components/ItemDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="head">
+      <h4>{{ items[$route.params.id].title }}</h4>
+      <p>{{ items[$route.params.id].date }}</p>
+    </div>
+    <p>{{ items[$route.params.id].content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemDetail",
+  props: {
+    items: Array,
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 90%;
+  margin-top: 50px;
+  height: 250px;
+}
+.head {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 80px;
+}
+</style>
