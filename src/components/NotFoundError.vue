537a1c8be983ed663f6b87d378516eabacdac30f
ansmeer008 <ansmeer008@gmail.com>
2023-04-15 11:37:27 +0900

    feat: NotFoundError Component

---
+++ src/components/NotFoundError.vue
@@ -0,0 +1,15 @@
+<template>
+  <h4 class="notfoundtext">404 Not Found!</h4>
+</template>
+
+<script>
+export default {
+  name: "NotFoundError",
+};
+</script>
+
+<style>
+.notfoundtext {
+  margin-top: 50px;
+}
+</style>
